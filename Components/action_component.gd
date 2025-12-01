0ca2a46f4e255772d9740555670108bcd86136a8
deadleegames <broomatt@live.com>
2025-11-30 22:39:22 -0600

    WIP Proj

--- Components/action_component.gd
+++ Components/action_component.gd
@@ -1,9 +1,21 @@
 extends Node
+class_name ActionComponent
+var Actions : Array[Action]
 
-#var Actions : Array[Action]
+var ActiveActionTags : Array[String]
+
+func _ready() -> void:
+	for child in get_children():
+		Actions.append(child as Action)
 
 func start_action_by_name(action_name: String):
-	pass
+	for action in Actions:
+		if action.ActionName == action_name:
+			action.start_action()
+			ActiveActionTags.append(action_name)
 
 func stop_action_by_name(action_name: String):
-	pass
+	for action in Actions:
+		if action.ActionName == action_name:
+			action.stop_action()
+			ActiveActionTags.erase(action_name)
